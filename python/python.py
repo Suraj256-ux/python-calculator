5b69ecf44e223a1f29e20a2e77a7fe6a79ae0d1b
Suraj Neupane <surajneupane537@gmail.com>
2025-06-19 18:03:46 +0545

    Create python.py

---
+++ python/python.py
@@ -0,0 +1,30 @@
+question=int(input("what so you want to do ?" 
+               "type 1 for addtion, 2 for subtraction , 3 for multiplication , 4 for division     :"))
+
+
+a= int(input("enter your first number"))
+b= int(input("enter your second  number"))
+
+# if user enter one we have to print the sum of a numbers
+
+if (question==1):
+    print("sum of two number is",a+b)
+
+    # if user enter one we have to print the subtractionof a numbers
+
+elif (question==2): 
+    print ("subtraction of a number is ",a-b)
+
+    # if user enter one we have to print the multiplication of a numbers
+
+elif(question==3):
+    print ("multiplication of a two number is ",a*b)
+
+    # if user enter one we have to print the division a numbers
+
+elif(question==3):
+    print ("multiplication of a two number is ",a*b)
+
+
+
+
