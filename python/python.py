5ef0356d477d51db747cf86568e98b60dcb9275b
Suraj Neupane <surajneupane537@gmail.com>
2025-06-20 12:12:48 +0545

    update python.py

--- python/python.py
+++ python/python.py
@@ -2,8 +2,8 @@ question=int(input("what so you want to do ?"
                "type 1 for addtion, 2 for subtraction , 3 for multiplication , 4 for division     :"))
 
 
-a= int(input("enter your first number"))
-b= int(input("enter your second  number"))
+a= int(input("Enter your first number :"))
+b= int(input("Enter your second  number  :"))
 
 # if user enter one we have to print the sum of a numbers
 
@@ -22,8 +22,8 @@ elif(question==3):
 
     # if user enter one we have to print the division a numbers
 
-elif(question==3):
-    print ("multiplication of a two number is ",a*b)
+elif(question==4):
+    print ("multiplication of a two number is ",a/b)
 
 
 
